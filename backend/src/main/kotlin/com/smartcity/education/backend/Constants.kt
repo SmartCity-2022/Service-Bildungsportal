e2783c2135329475f45791503c73b941b9e9d5b7
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2022-05-20 22:51:12 +0200

    introduce event for creation of education

---
+++ backend/src/main/kotlin/com/smartcity/education/backend/Constants.kt
@@ -0,0 +1,11 @@
+package com.smartcity.education.backend
+
+object Constants {
+    object Exchanges {
+        const val education = "education"
+    }
+
+    object RoutingKeys {
+        const val created = "service.education.created"
+    }
+}
--- backend/src/main/kotlin/com/smartcity/education/backend/controllers/LocationApiController.kt
+++ backend/src/main/kotlin/com/smartcity/education/backend/controllers/LocationApiController.kt
@@ -1,10 +1,12 @@
 package com.smartcity.education.backend.controllers
 
+import com.smartcity.education.backend.Constants
 import com.smartcity.education.backend.assigners.LocationAssigner
 import com.smartcity.education.backend.models.Education
 import com.smartcity.education.backend.models.Location
 import com.smartcity.education.backend.models.LocationProperties
 import com.smartcity.education.backend.repositories.LocationRepository
+import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
@@ -27,6 +29,9 @@ class LocationApiController {
 
     @Autowired
     private val assigner: LocationAssigner? = null
+
+    @Autowired
+    private val template: RabbitTemplate? = null
 
     @RequestMapping(
         method = [RequestMethod.GET],
@@ -61,7 +66,15 @@ class LocationApiController {
         return location?.let {
             education.location = it
             location.educations.add(education)
-            repository?.save(it)
+            val created = repository?.save(it)
+
+            created?.educations?.last()?.id?.let { id ->
+                template?.convertAndSend(
+                    Constants.Exchanges.education,
+                    Constants.RoutingKeys.created,
+                    id.toString()
+                )
+            }
 
             ResponseEntity
                 .created(URI("/location/${education.id}"))
